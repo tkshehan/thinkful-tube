98a056bb84f8c1ac5e5f0f4997a37a225f2879cb
Thomas Shehan <thomas.shehan88@gmail.com>
2018-07-19 11:07:39 -0500

    added style.css and clickable thumbnails

--- index.js
+++ index.js
@@ -21,20 +21,27 @@ function getDataFromApi(searchTerm) {
     key: 'AIzaSyCyzP05Kp56tTxNYzK_2zyey4Q2LwtkviY',
     q: searchTerm,
     part: 'snippet',
+    type: 'video',
+    maxResults: 8,
   };
 
   $.getJSON(YOUTUBE_SEARCH_URL, query, renderSearchResults);
 }
 
 function renderSearchResults(data) {
-  let thumbnails = data.items.map(function(item) {
-    return item.snippet.thumbnails.medium.url;
+  let items = data.items.map(function(item) {
+    return {
+      url: item.snippet.thumbnails.medium.url,
+      id: item.id.videoId,
+    }
   });
 
-  let html = thumbnails.map(function(url) {
+  let html = items.map(function(item) {
     return `
     <div class="col-3">
-      <img src="${url}" alt="Youtube Thumbnail">
+      <a href="https://www.youtube.com/watch?v=${item.id}">
+        <img src="${item.url}" alt="Youtube Thumbnail">
+      </a>
     </div>
     `
   });
